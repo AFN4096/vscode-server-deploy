147a81c0a3bade579d5d07e4975b8cebb0d049ab
Harris Xie <jiajun.xie.sh@gmail.com>
2021-08-27 19:48:13 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ commit_id=$1
 # Download url is: https://update.code.visualstudio.com/commit:${commit_id}/server-linux-x64/stable
 curl -sSL "https://update.code.visualstudio.com/commit:${commit_id}/server-linux-x64/stable" -o /tmp/vscode-server-linux-x64.tar.gz
 
+rm -rf ~/.vscode-server/bin/${commit_id}
 mkdir -p ~/.vscode-server/bin/${commit_id}
 
 # assume that you upload vscode-server-linux-x64.tar.gz to /tmp dir
